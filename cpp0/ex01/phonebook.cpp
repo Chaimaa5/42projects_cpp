26a8a05c130b5cfdc56f14af08fe88617aa31d90
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-07-01 17:07:43 +0100

    updates

--- cpp0/ex01/phonebook.cpp
+++ cpp0/ex01/phonebook.cpp
@@ -1,19 +1,21 @@
 #include "phonebook.hpp"
 
-int main()
+void    make_a_choice(void) 
 {
-    char msg[512];
-    Contact contact;
-
     std::cout << "Please enter your request ADD, SEARCH or EXIT:" << std::endl;
     std::cout << "1) ADD" << std::endl;
     std::cout << "2) SEARCH" << std::endl;
     std::cout << "3) EXIT" << std::endl;
-    std::cin >> msg;
-    if (!strcmp(msg, "EXIT"))
-        std::cout << "See you next time" << std::endl;
-    else if (!strcmp(msg, "ADD"))
-    {
+}
+
+void    program_exit(void)
+{
+    std::cout << "See you next time" << std::endl;
+    return ;
+}
+
+void    add_contact(Contact contact)
+{
         std::cout << "Please enter contact's name:" << std::endl;
         std::cin >> contact.name;
         std::cout << "Please enter contact's age:" << std::endl;
@@ -21,17 +23,32 @@ int main()
         std::cout << "Please enter contact's phone number:" << std::endl;
         std::cin >> contact.phone;
         std::cout << "Contact added successfuly!" << std::endl;
-    }
-    else if (!strcmp(msg, "SEARCH"))
-    {
-        std::cout << "Please enter id of contact:" << std::endl;
-        std::cin >> msg;
-        // Search Function
-        std::cout << "Contact Details " << std::endl;
-	    std::cout << "Name: " << std::endl;
-	    std::cout << "Age:  " << std::endl;
-	    std::cout << "Phone number:  " << std::endl;    
-    }
+}
+
+void    search_contact(char *msg)
+{
+    std::cout << "Please enter id of contact:" << std::endl;
+    std::cin >> msg;
+    // Search Function
+    std::cout << "Contact Details " << std::endl;
+	std::cout << "Name: " << std::endl;
+	std::cout << "Age:  " << std::endl;
+	std::cout << "Phone number:  " << std::endl;  
+}
+
+int main()
+{
+    char msg[512];
+    Contact contact;
+
+    make_a_choice();
+    std::cin >> msg;
+    if (!strcmp(msg, "EXIT") || !strcmp(msg, "3"))
+        program_exit();
+    else if (!strcmp(msg, "ADD") || !strcmp(msg, "1"))
+        add_contact(contact);
+    else if (!strcmp(msg, "SEARCH") || !strcmp(msg, "2"))
+        search_contact(msg);
     else
         std::cout << "Sorry we didn't recognize your request!" << std::endl;
     return (0);
