9404afcb8b078d0c470c087b2d74aaeee079c7cb
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-07-01 15:57:08 +0100

    updates

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,11 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+class Zombie{
+    std::string name;
+    public:
+        Zombie::Zombie();
+        Zombie::~Zombie();
+    void announce(void);
+}
+#endif
