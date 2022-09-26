be07396f4ccc4c86bc5de2936ac03711a97e91a1
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-09-26 22:35:50 +0100

    cpp01

--- cpp01/ex01/main.cpp
+++ cpp01/ex01/main.cpp
@@ -10,5 +10,8 @@ int main()
     Zombie *zombie;
 
     zombie = zombieHorde(10, "zombie");
-    delete(zombie);
+    for (int i = 0; i < 10; i++)
+        zombie[i].announce();
+    delete [] zombie;
+    return (0);
 }
