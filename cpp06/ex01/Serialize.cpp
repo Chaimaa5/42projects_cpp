4d0c043d0dabd319aceae5d36217139875d2dbda
Chaimaa5 <Chaimaa.elmhandez@gmail.com>
2022-11-20 15:10:37 +0000

    06

---
+++ cpp06/ex01/Serialize.cpp
@@ -0,0 +1,8 @@
+#include "Serialize.hpp"
+
+uintptr_t serialize(Data* ptr){
+    
+}
+Data* deserialize(uintptr_t raw){
+
+}
---
+++ cpp06/ex01/Serialize.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+
+struct Data{
+    
+}
+uintptr_t serialize(Data* ptr);
+Data* deserialize(uintptr_t raw);
