01d2fbe42f623701c4465b28f2077c6b77522e99
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-11-20 02:55:52 +0100

    06

---
+++ cpp06/ex00/Convert.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <exception>
+#include <limits>
+
+int ConvertToInt(std::string ar);
+char ConvertToChar(std::string ar);
+float ConvertToFloat(std::string ar);
