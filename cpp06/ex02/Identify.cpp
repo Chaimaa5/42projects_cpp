6001b7de6b00362b4f4a43c952f808746cf89069
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-11-20 18:43:07 +0100

    0/02

---
+++ cpp06/ex02/Identify.cpp
@@ -0,0 +1,25 @@
+#include "Identify.hpp"
+
+Base* generate(void){
+    Base* ptr;
+
+    srand(time(NULL));
+    int random =  1 +(rand() % 3);
+
+    if (random == 1)
+        ptr = new A;
+    if (random == 2)
+        ptr = new B;
+    if (random == 3)
+        ptr = new C;
+    return (ptr);
+}
+
+// void identify(Base* p){
+
+
+// }
+
+// void identify(Base& p){
+
+// }
---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,7 @@
+#include "Identify.hpp"
+
+int main()
+{
+    
+    return 0;
+}
