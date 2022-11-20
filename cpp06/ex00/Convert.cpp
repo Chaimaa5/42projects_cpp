a96e6802e81ac34535aa0658130ca87d34250cfb
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-11-20 17:49:30 +0100

    06

--- cpp06/ex00/Convert.cpp
+++ cpp06/ex00/Convert.cpp
@@ -1,26 +1,31 @@
 #include "Convert.hpp"
 
 void    ConvertToChar(std::string ar){
-    std::cout << "char: ";
-    try{
-        if (ar.length() == 1)
-        {
-            char c = ar[0];
-            std::cout << c << std::endl;
-            throw ();
-        }
+    std::cout << "Char: ";
+    if (ar.length() == 1 && !std::isdigit(ar[0]))
+    {
+        std::cout << ar[0] << std::endl;
+        return;
     }
-    catch(const std::exception & e){
+    try{
+        int c = std::stoi(ar);
+        if (c < 0 || c > 255)
+            throw std::exception();
+        if (!std::isprint(c))
+            std::cout << "Non displayable\n";
+        else
+            std::cout << static_cast<char>(c) << std::endl;
+    }
+    catch(const std::exception &e){
         std::cout << "Impossible" << std::endl;
     }
 }
 
 void ConvertToInt(std::string ar){
-    std::cout << "int: ";
+    std::cout << "Int: ";
     try{
         int n = stoi(ar);
-        std::cout << n << std::endl;
-        throw ();
+        std::cout << static_cast<int>(n) << std::endl;
     }
     catch(const std::exception & e){
         std::cout << "Impossible" << std::endl;
@@ -28,11 +33,21 @@ void ConvertToInt(std::string ar){
 }
 
 void   ConvertToFloat(std::string ar){
-    std::cout << "float: ";
+    std::cout << "Float: ";
     try{
         float f = stof(ar);
         std::cout << static_cast<float>(f) << "f" << std::endl;
-        throw ();
+    }
+    catch(const std::exception & e){
+        std::cout << "Impossible" << std::endl;
+    }
+}
+
+void   ConvertToDouble(std::string ar){
+    std::cout << "Double: ";
+    try{
+        double f = stod(ar);
+        std::cout << static_cast<double>(f) << std::endl;
     }
     catch(const std::exception & e){
         std::cout << "Impossible" << std::endl;
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -4,9 +4,12 @@ int main(int ac, char **av)
 {
     if (ac == 2)
     {
-        std::string ar = av[1];
+        std::cout.precision(1);
+		std::cout << std::fixed;
+        std::string ar = std::string(av[1]);
         ConvertToChar(ar);
         ConvertToInt(ar);
         ConvertToFloat(ar);
+        ConvertToDouble(ar);
     }
 }
