34a3a9299ed86e783d5d4bd8dce34dd5caf5ec16
Chaimaa5 <chaimaa.elmhandez@gmail.com>
2022-11-27 15:04:33 +0100

    08

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,37 +1,5 @@
 #include "MutantStack.hpp"
 
-// int main()
-// {
-// 	MutantStack<int> mstack;
-// 	mstack.push(18);
-// 	mstack.push(17);
-// 	std::cout << mstack.top() << std::endl;
-// 	mstack.pop();
-// 	std::cout << mstack.size() << std::endl;
-// 	mstack.push(3);
-// 	mstack.push(5);
-// 	mstack.push(737);
-// 	//[...]
-// 	mstack.push(0);
-// 	MutantStack<int>::iterator it = mstack.begin();
-// 	MutantStack<int>::iterator ite = mstack.end();
-
-// 	// MutantStack<int>::reverse_iterator rev = mstack.rbegin();
-// 	// MutantStack<int>::reverse_iterator reve = mstack.rend();
-
-// 	// ++rev;
-// 	// --reve;
-// 	// ++it;
-// 	// --it;
-// 	while (it != ite)
-// 	{
-// 	std::cout << *it << std::endl;
-// 	++it;
-// 	}
-// 	std::stack<int> s(mstack);
-// 	return 0;
-// }
-
 int main()
 {
 	MutantStack<int> mstack;
@@ -54,9 +22,33 @@ int main()
 
 	while (rev != reve)
 	{
-	std::cout << *rev << std::endl;
-	++rev;
+		std::cout << *rev << std::endl;
+		++rev;
+	}
+
+	MutantStack<int> test(mstack);
+	test.push(900);
+	test.push(233);
+	std::cout << test.top() << std::endl;
+	std::cout << test.size() << std::endl;
+	test.push(334);
+	test.push(4);
+	test.push(800);
+	test.pop();
+	test.push(765);
+	test.push(435);
+
+	MutantStack<int>::iterator it = test.begin();
+	MutantStack<int>::iterator rit = test.end();
+
+	++it;
+	--it;
+	while (it != rit)
+	{
+		std::cout << *it << std::endl;
+		++it;
 	}
 	std::stack<int> s(mstack);
+	
 	return 0;
 }
